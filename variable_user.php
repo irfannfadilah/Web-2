4b0cd33776ba3e5b87024297bfdae38082b815e3
irfannfadilah <123irfanfadilah06@gmail.com>
2025-02-24 12:06:55 +0700

    praktikum 1

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variable
+$nama = 'Irfan';
+$umur = 19;
+$berat = 60.60;
+
+echo 'Nama saya ' . $nama;
+echo 'br/>Umur :' . $umur . ' Tahun';
+echo 'br/Berat :' . $berat;
+
+?>
