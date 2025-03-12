56441dd61c4826ff1342b0909bb58c0192169b88
irfannfadilah <123irfanfadilah06@gmail.com>
2025-03-12 17:47:28 +0700

    praktikum03

---
+++ praktikum/partials/modals/index.php
@@ -0,0 +1,8 @@
+<?php 
+include_once 'portofolio-1.php'; 
+include_once 'portofolio-2.php'; 
+include_once 'portofolio-3.php'; 
+include_once 'portofolio-4.php'; 
+include_once 'portofolio-5.php'; 
+include_once 'portofolio-6.php'; 
+?>
---
+++ task/partials/sections/about.php
@@ -0,0 +1,18 @@
+<section class="bg-light py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-xxl-8">
+                            <div class="text-center my-5">
+                                <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                                <p class="lead fw-light mb-4">I am Muhamad Irfan Fadilah and I am a expert Data Analyst who loves to processing data.</p>
+                                <p class="text-muted">I am a Data Analyst professional with expertise in processing, analyzing, and interpreting data to generate valuable business insights. I am proficient in using data analysis tools such as Python, SQL, and Excel, and have a strong understanding of data visualization and statistical techniques. With a data-driven approach, I can assist companies in making more effective and strategic decisions.</p>
+                                <div class="d-flex justify-content-center fs-2 gap-4">
+                                    <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
+                                    <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
+                                    <a class="text-gradient" href="#!"><i class="bi bi-github"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
