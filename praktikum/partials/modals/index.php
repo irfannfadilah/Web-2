0478dd9dcedb2ca6f185b3138e28f6a0c8a99b2c
irfannfadilah <123irfanfadilah06@gmail.com>
2025-03-13 10:38:04 +0700

    praktikum04

--- praktikum/partials/modals/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once 'portofolio-1.php'; 
-include_once 'portofolio-2.php'; 
-include_once 'portofolio-3.php'; 
-include_once 'portofolio-4.php'; 
-include_once 'portofolio-5.php'; 
-include_once 'portofolio-6.php'; 
-?>
