86f0e3987bb350462abc4af6cfbcc725a8b1fa64
irfannfadilah <123irfanfadilah06@gmail.com>
2025-04-23 16:02:50 +0700

    praktikum06

--- connection.php
+++ connection.php
@@ -1,15 +1,21 @@
 <?php 
-$host = 'localhost';
-$db = 'learn_pdo';
-$user = 'root';
-$password = '';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+require_once 'config.php';
 
-try {
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    
-    echo $e->getMessage();
+class connection
+{
+    public static function make($host, $db, $user, $password){
+        $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+        try {
+            $options =[PDO:: ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+            return new PDO($dsn, $user, $password, $options);
+    }   catch (PDOException $e) {
+         die($e->getMessage());
+
+        }
+            
+    }
 }
+
+return Connection::make($host, $db, $user, $password);
